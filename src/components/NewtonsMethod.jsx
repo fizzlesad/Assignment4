0d3d95c5a5b547f52ab2ec768782329810a30aa9
Richard <richard.li12@student.tdsb.on.ca>
2024-11-17 22:35:21 -0500

    attempted fix on Newton's Method

--- src/components/NewtonsMethod.jsx
+++ src/components/NewtonsMethod.jsx
@@ -4,18 +4,19 @@ import './NewtonsMethod.css'
 function NewtonsMethod() {
 
     const [g, gValue] = useState(0);
-    const [type, setType] = useState("");
+    const [root, setRoot] = useState("");
 
     function newtonsMethod(e) {
         e.preventDefault();
         let root = (g - (6 * Math.pow(g, 4) - 13 * Math.pow(g, 3) - 18 * Math.pow(g, 2) + 7 * g + 6) / (24 * Math.pow(g, 3) - 39 *
             Math.pow(g, 2) - 36 * g + 7));
-        while (root - g > 0.000001) {
-            gValue(root);
-            root = (g - (6 * Math.pow(g, 4) - 13 * Math.pow(g, 3) - 18 * Math.pow(g, 2) + 7 * g + 6) / (24 * Math.pow(g, 3) - 39 *
-                Math.pow(g, 2) - 36 * g + 7));
+        let guess = g;
+        while (root - guess != 0) {
+            guess = root;
+            root = (guess - (6 * Math.pow(guess, 4) - 13 * Math.pow(guess, 3) - 18 * Math.pow(guess, 2) + 7 * g + 6) / (24 * Math.pow(guess, 3) - 39 *
+                Math.pow(guess, 2) - 36 * guess + 7));
         }
-        setType(root);
+        setRoot(root);
     }
 
     return (
@@ -24,7 +25,7 @@ function NewtonsMethod() {
             <label>Root Guess:</label>
             <input type="number" value={g} onChange={(event) => { gValue(event.target.value) }} required />
             <label>Root Approximation (Result):</label>
-            <input type="text" value={type} readOnly />
+            <input type="text" value={root} readOnly />
             <input type="submit" value="Calculate" />
         </form>
     )
